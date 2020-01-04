c69ebd517defb85a2c59724aebc8c80de379d9aa
Ivan Phelps <ivanniothemannio@gmail.com>
2020-01-04 10:03:44 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center">
+      <button className="btn btn-outline-warning">8U770N</button>
+    </div>
+  );
+}
+
+export default App;
