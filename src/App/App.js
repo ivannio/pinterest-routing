0c7d176941f6673c7f3a352383a34a276b595464
Ivan Phelps <ivanniothemannio@gmail.com>
2020-01-13 19:13:33 -0600

    create boards complete

--- src/App/App.js
+++ src/App/App.js
@@ -12,7 +12,7 @@ import firebaseConnection from '../helpers/data/connection';
 import MyNavBar from '../components/shared/MyNavBar/MyNavBar';
 import Auth from '../components/pages/Auth/Auth';
 import Home from '../components/pages/Home/Home';
-import NewBoard from '../components/pages/NewBoard/NewBoard';
+import BoardForm from '../components/pages/BoardForm/BoardForm';
 import SingleBoard from '../components/pages/SingleBoard/SingleBoard';
 import './App.scss';
 
@@ -54,7 +54,7 @@ class App extends React.Component {
           <MyNavBar authed={authed}/>
           <Switch>
             <PrivateRoute path="/" exact component={Home} authed={authed} />
-            <PrivateRoute path="/board/new" exact component={NewBoard} authed={authed} />
+            <PrivateRoute path="/board/new" exact component={BoardForm} authed={authed} />
             <PublicRoute path="/auth" exact component={Auth} authed={authed} />
             <PrivateRoute path="/board/:boardId" exact component={SingleBoard} authed={authed} />
           </Switch>
